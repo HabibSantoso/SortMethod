1d42dc4f4c3ca3968a76b4ac74f929997989e1ae
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-10-07 18:39:54 +0700

    add bubble sort array method

--- SortMethod/Program.cs
+++ SortMethod/Program.cs
@@ -55,7 +55,22 @@ namespace SortMethod
             Console.WriteLine("");
         }
 
-        
+        public void BubbleSortArray()
+        {
+            for (int i = 0;i < n; i++)
+            {
+                for(int j = 0; j<n-1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
