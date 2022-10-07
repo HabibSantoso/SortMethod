7043c10191f2d1893be08450576ae7a7e9b15783
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-10-07 17:33:21 +0700

    add methtod read

--- SortMethod/Program.cs
+++ SortMethod/Program.cs
@@ -8,8 +8,38 @@ namespace SortMethod
 {
     class Program
     {
-        private int[] a = new int[7];
+        private int[] a = new int[20];
         private int n;
+
+        public void read()
+        {
+            while(true)
+            {
+                Console.Write("Masukkan banyak elemen pada array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen. \n");
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Masukan elemen array");
+                Console.WriteLine("----------------------------");
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("<" + (i + 1) + "> ");
+                    string s1 = Console.ReadLine();
+                    a[i] = Int32.Parse(s1);
+                } 
+            }
+        }
 
         static void Main(string[] args)
         {
